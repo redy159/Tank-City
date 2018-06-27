a4b415da3d6ba4ad8a7eb2146d52741d53216b19
ScaryLittleOne <ngToan1107@gmail.com>
2018-06-27 19:29:47 +0700

    Fix FirePosition Of Tanks And Add An Enemy Detection Function

---
+++ Assets/scripts/Trigger_Fire.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Fire : Tank {
+
+	// Use this for initialization
+	 public Rigidbody2D rb;
+
+	 void FreeFire(){
+
+		 
+	 }
+	void TriggerFire(){
+		//Tinh Chinh Huong Quay Mat Cua Tank
+		dx = (firePosition.position.x - transform.position.x);
+		dy = (firePosition.position.y - transform.position.y);
+		dx = (dx > dy) ? 1 : 0;
+		if (dx > 0) 
+			dx *= 1;
+		else dx *= -1;
+		
+		dy = (dx < dy) ? 1 : 0;
+		if (dy > 0) 
+			dy *= 1;
+		else dy *= -1;
+
+		//Ray Cast Theo Huong Quay
+		RaycastHit2D hit = Physics2D.Raycast(firePosition.position, new Vector2(dx,dy) );
+		float R = 10; // Range
+        
+		if (hit.collider != null) {
+			if (hit.rigidbody.tag == "Player"){ 
+                Debug.Log("Pew");
+				float DX = Mathf.Abs(hit.rigidbody.position.x - transform.position.x);
+				float DY = Mathf.Abs(hit.rigidbody.position.y - transform.position.y);
+				if (((DX <= R) && (dx != 0)) || ((DY <= R) && (dy != 0)) ){ //Kiem tra Muc Tieu Da Trong Tam ban R khong
+					//this.Shoot(); //Ban Lien Tuc
+				}
+            }
+	    }
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
