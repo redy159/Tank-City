497abafd2d5010e09b8f8af12ccfb1a60aa1b278
thuanpham1206 <39146788+thuanpham1206@users.noreply.github.com>
2018-07-02 16:57:40 +0700

    Done About Scene

--- Assets/scripts/About.cs
+++ Assets/scripts/About.cs
@@ -11,6 +11,9 @@ public class About : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.anyKeyDown)
+        {
+            Application.LoadLevel("Menu_Demo");
+        }
 	}
 }
